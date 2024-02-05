494d762e068dc983ef9a0a241bbf592963f1090c
Amal Mokhtar <amalmokhtar@icloud.com>
2024-02-05 13:56:09 +0200

    Connect Data to KafkaProducer

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -8,14 +8,42 @@ default_args = {
 }
 
 
-def stream_data():
-    import json
+def get_data():
     import requests
+    import json
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
-    print(json.dumps(res,indent=3))
+    return res
 
+
+def format_data(res):
+    data = {}
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['adddress'] = f"{str(res['location']['street']['number'])} {res['location']['street']['name']}, " \
+                       f"{res['location']['city']}, {res['location']['state']}, {res['location']['country']}"
+    data['post_code'] = res['location']['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+    return data
+
+
+def stream_data():
+    import json
+    from kafka import KafkaProducer
+    raw = get_data()
+    data = format_data(raw)
+
+    producer = KafkaProducer(bootstrap_servers="localhost:9092", max_block_ms=5000)
+
+    producer.send('user_details', json.dumps(data).encode('utf-8'))
+    print(json.dumps(data, indent=3))
 
 
 # with DAG('user_automation',
